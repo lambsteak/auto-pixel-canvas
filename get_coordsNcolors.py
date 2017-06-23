babc69d2a573a9fa9963b3e74167b6b7481a4f1c
Param Prabhakar <prabhakarparam@gmail.com>
2017-06-23 17:17:24 +0530

    Update get_coordsNcolors.py

--- get_coordsNcolors.py
+++ get_coordsNcolors.py
@@ -1,21 +1,15 @@
-'''
-Created on 21-Jun-2017
-
-@author: thepenguin
-'''
-
 import pyautogui
 
 try:
     while True:
         x, y = pyautogui.position()
-        positionStr = 'Coordinate: (' + str(x).rjust(4) + ', ' + str(y).rjust(4) + ')  '
+        pos_str = 'Coordinate: (' + str(x).rjust(4) + ', ' + str(y).rjust(4) + ')  '
         
         pixelColor = pyautogui.screenshot().getpixel((x, y))
-        positionStr += ' RGB: (' + str(pixelColor[0]).rjust(3)
-        positionStr += ', ' + str(pixelColor[1]).rjust(3)
-        positionStr += ', ' + str(pixelColor[2]).rjust(3) + ')'
-        print(positionStr, end='')
-        print('\b' * len(positionStr), end='', flush=True)
+        pos_str += ' RGB: (' + str(pixelColor[0]).rjust(3)
+        pos_str += ', ' + str(pixelColor[1]).rjust(3)
+        pos_str += ', ' + str(pixelColor[2]).rjust(3) + ')'
+        print(pos_str, end='')
+        print('\b' * len(pos_str), end='', flush=True)
 except KeyboardInterrupt:
     print('\nDone.')
